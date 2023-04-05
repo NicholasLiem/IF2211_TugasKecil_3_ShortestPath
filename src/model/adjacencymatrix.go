b682915a9760cc7050f2a09ec5d5b84766d9707e
energizedNachos <16521108@mahasiswa.itb.ac.id>
2023-04-05 16:27:54 +0700

    + adding models

---
+++ src/model/adjacencymatrix.go
@@ -0,0 +1,29 @@
+package model
+
+type adjacencyMatrix struct {
+	matrix       [][]int64
+	columnLabels []string
+	nodesCount   int
+}
+
+func (am adjacencyMatrix) getNodesCount() int {
+	return am.nodesCount
+}
+
+func newAdjacencyMatrix(nodeCount int, columnLabels []string) adjacencyMatrix {
+	matrix := make([][]int64, nodeCount)
+	for i := range matrix {
+		matrix[i] = make([]int64, nodeCount)
+	}
+
+	return adjacencyMatrix{
+		matrix:       matrix,
+		columnLabels: columnLabels,
+		nodesCount:   nodeCount,
+	}
+}
+
+func testing() {
+	//am := newAdjacencyMatrix(10)
+	//fmt.Println(am.getNodesCount())
+}
