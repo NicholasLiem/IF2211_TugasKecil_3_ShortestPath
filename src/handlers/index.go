3899bd77d7c9366b1fd8337458c1c6299f8ac916
energizedNachos <16521108@mahasiswa.itb.ac.id>
2023-04-11 17:10:25 +0700

    docs: restructuring handlers

---
+++ src/handlers/index.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,62 +1,16 @@
 package main
 
 import (
-	"html/template"
-	"io"
+	"github.com/NicholasLiem/IF2211_TugasKecil_3_RoutePlanning/handlers"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
-type PageData struct {
-	FileName string
-}
-
 func main() {
-	http.HandleFunc("/", indexHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/", handlers.IndexHandler)
+	http.HandleFunc("/upload", handlers.UploadHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
 	}
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	err := tmpl.Execute(w, nil)
-	if err != nil {
-		return
-	}
-}
-
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	const MaxMultipartMemory = 100 << 20 // 100MB
-	r.ParseMultipartForm(MaxMultipartMemory)
-
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		http.Redirect(w, r, "/?uploaded=false&message=File upload failed", http.StatusSeeOther)
-		return
-	}
-	defer file.Close()
-
-	ext := filepath.Ext(header.Filename)
-
-	switch ext {
-	case ".txt":
-	default:
-		http.Redirect(w, r, "/?uploaded=false&message=File upload failed", http.StatusSeeOther)
-		return
-	}
-
-	f, err := os.OpenFile("./uploads/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		http.Redirect(w, r, "/?uploaded=false&message=File upload failed", http.StatusSeeOther)
-		return
-	}
-	defer f.Close()
-
-	io.Copy(f, file)
-	http.Redirect(w, r, "/?uploaded=true&message=File uploaded successfully", http.StatusSeeOther)
 }
