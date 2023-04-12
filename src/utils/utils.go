4fd443637307cf2a22419b882e911e08b2007021
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-04-12 10:47:31 +0700

    fix: algorithm bugs
    - use Euclidean distance instead of haversine
    - split node entry by comma instead of whitespace

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -7,10 +7,8 @@ import (
 	"strings"
 )
 
-// TODO: proper parser
-
 func ParseNode(line string) (string, float64, float64, error) {
-	words := strings.Split(line, " ")
+	words := strings.Split(line, ",")
 	name := strings.TrimSpace(words[0])
 
 	latitude, err := strconv.ParseFloat(strings.TrimSpace(words[1]), 64)
@@ -45,11 +43,7 @@ func ParseRow(line string, columns int) ([]float64, error) {
 }
 
 func Distance(lat1, lon1, lat2, lon2 float64) float64 {
-	p := 0.017453292519943295 // Math.PI / 180
-	c := math.Cos
-	a := 0.5 - c((lat2-lat1)*p)/2 +
-		c(lat1*p)*c(lat2*p)*
-			(1-c((lon2-lon1)*p))/2
-
-	return 12742 * math.Asin(math.Sqrt(a)) // 2 * R; R = 6371 km
+	deltaX := lon2 - lon1
+	deltaY := lat2 - lat1
+	return math.Sqrt(deltaX*deltaX + deltaY*deltaY)
 }
