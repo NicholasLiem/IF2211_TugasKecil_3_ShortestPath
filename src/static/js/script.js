5900620a3866ff736a0e7a8b515276f4a6a724c5
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-04-12 13:03:55 +0700

    feat: text wrapping

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -110,7 +110,10 @@ function drawText(text, x, y, ctx) {
     ctx.font = "15px Poppins";
     ctx.textAlign = "center";
     ctx.fillStyle = "#fff"
-    ctx.fillText(text, x, y)
+    const wrappedText = wrapText(ctx, text, x, y, nodeRadius * 20, 15);
+    wrappedText.forEach((item) => {
+        ctx.fillText(item[0], item[1], item[2] - 7.5 * (wrappedText.length - 1));
+    })
 }
 
 function drawLine(x1, y1, x2, y2, color, ctx) {
@@ -119,6 +122,43 @@ function drawLine(x1, y1, x2, y2, color, ctx) {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
+}
+
+// source: https://fjolt.com/article/html-canvas-how-to-wrap-text
+function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
+    // First, start by splitting all of our text into words, but splitting it into an array split by spaces
+    let words = text.split(' ');
+    let line = ''; // This will store the text of the current line
+    let testLine = ''; // This will store the text when we add a word, to test if it's too long
+    let lineArray = []; // This is an array of lines, which the function will return
+
+    // Lets iterate over each word
+    for(var n = 0; n < words.length; n++) {
+        // Create a test line, and measure it..
+        testLine += `${words[n]} `;
+        let metrics = ctx.measureText(testLine);
+        let testWidth = metrics.width;
+        // If the width of this test line is more than the max width
+        if (testWidth > maxWidth && n > 0) {
+            // Then the line is finished, push the current line into "lineArray"
+            lineArray.push([line, x, y]);
+            // Increase the line height, so a new line is started
+            y += lineHeight;
+            // Update line and test line to use this word as the first word on the next line
+            line = `${words[n]} `;
+            testLine = `${words[n]} `;
+        }
+        else {
+            // If the test line is still less than the max width, then add the word to the current line
+            line += `${words[n]} `;
+        }
+        // If we never reach the full max width, then there is only one line.. so push it into the lineArray so we return something
+        if(n === words.length - 1) {
+            lineArray.push([line, x, y]);
+        }
+    }
+    // Return the line array
+    return lineArray;
 }
 
 visualizer.style.width = "100%";
